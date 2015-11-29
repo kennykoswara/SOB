9581f37d1794a5929911f63eda0022fc4f3b7211
jegson <jegul95@gmail.com>
2015-11-29 18:54:32 +0700

    delete useless panel from profile and home

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -159,7 +159,7 @@
 								<div class="col-sm-7">
 
 									<div class="well">
-										<h4>What's Happened</h4>
+										<h4>Help Requested by Me</h4>
 										<br/>
 
 										<?php foreach ($post->result() as $row)
@@ -213,31 +213,6 @@
 				</div>
 			</div>
 
-
-			<!--post modal-->
-			<div id="postModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-							Update Status
-						</div>
-						<div class="modal-body">
-							<form class="form center-block">
-								<div class="form-group">
-									<textarea class="form-control input-lg" autofocus="" placeholder="What do you want to share?"></textarea>
-								</div>
-							</form>
-						</div>
-						<div class="modal-footer">
-							<div>
-								<button class="btn btn-primary btn-sm" data-dismiss="modal" aria-hidden="true">Post</button>
-								<ul class="pull-left list-inline"><li><a href=""><i class="glyphicon glyphicon-upload"></i></a></li><li><a href=""><i class="glyphicon glyphicon-camera"></i></a></li><li><a href=""><i class="glyphicon glyphicon-map-marker"></i></a></li></ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
 			<!-- script references -->
 			<script src="<?php echo base_url();?>asset/js/jquery-2.1.4.min.js" type="text/javascript"></script>
 			<script src="<?php echo base_url();?>asset/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
